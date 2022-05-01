0ce406bebab384e7763b150ef96ab9aeb66f0b21
Rose <rosesophiewright@gmail.com>
2022-04-30 19:43:16 -0500

    h1 access improvements

--- src/components/Home/Home components/CodePagePreview.js
+++ src/components/Home/Home components/CodePagePreview.js
@@ -9,7 +9,7 @@ class CodePagePreview extends Component{
                 <a href='index.html'>
                         <div className='Icon group hover:bg-slate-100 transition-all duration-200 ease-linear shadow-lg m-[3%] w-auto h-auto bg-slate-300 flex rounded-3xl p-5 flex-col'>
 
-                        <h1 className='Icon group-hover:bg-slate-200 Icon group-hover:text-rose-500 group-hover:rounded-3xl transition-all duration-200 ease-linear ml-auto mt-0 relative flex items-center justify-center text-right text-[4.5vw]  px-3 mb-2 bg-slate-200 text-slate-700 shadow-lg rounded-lg '>Computer Science</h1>
+                        <h1 className='Icon group-hover:bg-slate-200 Icon group-hover:text-rose-700 group-hover:rounded-3xl transition-all duration-200 ease-linear ml-auto mt-0 relative flex items-center justify-center text-right text-[4.5vw]  px-3 mb-2 bg-slate-200 text-slate-700 shadow-lg rounded-lg '>Computer Science</h1>
 
                         <img alt='Red Waves by Rose Wright is an art piece defined by its wave like sin and cos function visualized with Three.js' src={redwavesbyRoseWright}
                         
