ad23fd305bd57ce95b93645683ac2b65046f21e7
Rose <rosesophiewright@gmail.com>
2022-05-02 19:27:17 -0500

    Ui update

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -13,7 +13,7 @@ class Landing extends Component{
                             <br/>
                             <p className='text-[2.3vw]'>I help people build interactive and compelling user interfaces. I work with a strong focus on simplicity, accessibility and responsive design.</p>
                             <br/>
-                            <a href='index.html' className='underline text-rose-700 italic bg-rose-100 p-[0.5vw] rounded-sm border-2 border-rose-300 hover:border-rose-400 transition-all duration-200 ease-linear shadow-lg whitespace-nowrap	text-[2vw]'> Read the full article</a>
+                            <a href='index.html' className='underline text-rose-700 italic whitespace-nowrap text-[2vw]'> Read the full article</a>
 
                         </div>
                         
