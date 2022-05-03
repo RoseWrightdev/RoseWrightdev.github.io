04a1ae176176a4de228d3901b014239cb65c115b
rosepetaldrift <rosesophiewright@gmail.com>
2022-05-02 20:40:31 -0500

    endash added

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -9,7 +9,7 @@ class Landing extends Component{
                 <div className='transition-all duration-200 ease-linear shadow-lg mx-auto max-w-[70vw]   m-[3%] w-auto bg-slate-200 flex rounded-3xl p-[1vw] flex-row-reverse'>
                     <div className=''>
                         <div className='bg-slate-100 p-5 rounded-3xl shadow-lg '>
-                            <h1 className='text-[3vw]	'>Hi, I'm Rose -- UI and design system developer.</h1>
+                            <h1 className='text-[3vw]	'>Hi, I'm Rose – UI and design system developer.</h1>
                             <br/>
                             <p className='text-[2.3vw]'>I help people build interactive and compelling user interfaces. I work with a strong focus on simplicity, accessibility and responsive design.</p>
                             <br/>
