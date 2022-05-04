c6ddb981d95bb5b7bab6ab5d860cbfa20f63c853
rosepetaldrift <rosesophiewright@gmail.com>
2022-05-03 19:47:13 -0500

    UI update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,24 +3,24 @@ class Footer extends Component{
     render() {
         return (
             <div>
-                <div className='bg-slate-200 w-auto ml-16 min-h-[20vw]'>
-                    <div className='bg-slate-400 w-auto h-[0.05vw]'></div>
-                        <div className='flex flex-row m-[5vw] mt-[3vw] mb-[1vh] justify-evenly'>
+                <div className='bg-slate-200 w-auto ml-16 p-1'>
+                        <div className='flex flex-row m-[5vw] mt-[3vw] mb-[3vh] justify-evenly'>
+
                             <div className='rounded-lg  bg-slate-100 w-[33vw] h-auto mx-auto mx-1 text-center'>
-                                <h1 className='text-[2.3vw] '>About</h1>
+                                <h1 className='text-[2.3vw] text-rose-700 '>About</h1>
                                 < hr/>
-                                <p className='text-[1.5vw] text-center p-[0.5vw]'> All rights reserved.<br/>©2022</p>
+                                <p className='text-[1.5vw] text-center p-[0.5vw] text-slate-500'> All rights reserved.<br/>©2022</p>
                             </div>
                             <div className='rounded-lg bg-slate-100 w-[50vw] h-auto mx-auto mx-1 text-center'>
-                                <h1 className='text-[2.3vw]'>Contact</h1>
+                                <h1 className='text-[2.3vw]  text-rose-700 '>Contact</h1>
                                 < hr/>
-                                <p className='text-[1.5vw] text-center p-[0.5vw]'>RoseSophieWright@gmail.com <br/></p>
+                                <p className='text-[1.5vw] text-center p-[0.5vw] text-slate-500'>RoseSophieWright@gmail.com <br/></p>
 
                             </div>
                             <div className='rounded-lg bg-slate-100 w-[33vw] h-auto mx-auto mx-1 text-center'>
-                                <h1 className='text-[2.3vw]'>Privacy</h1>
+                                <h1 className='text-[2.3vw]  text-rose-700 '>Privacy</h1>
                                 < hr/>
-                                <p className='text-[1.5vw] text-center p-[0.5vw]'>RoseWright.dev dose not use tracking software. <br/> <a className='underline text-rose-700 italic whitespace-nowrap hover:bg-rose-100'  href='https://github.com/rosepetaldrift/rosepetaldrift.github.io'>Source</a> </p>
+                                <p className='text-[1.5vw] text-center p-[0.5vw] text-slate-500'>RoseWright.dev dose not use tracking software. <br/> <a className='underline text-rose-500 italic whitespace-nowrap hover:bg-rose-100'  href='https://github.com/rosepetaldrift/rosepetaldrift.github.io'>Source</a> </p>
                             </div>
                         </div>
                 </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import { MdCode, MdPalette, MdMode, MdAccountCircle } from "react-icons/md";
 import { IoMdRose } from "react-icons/io";
+import { NavLink } from "react-router-dom";
+
 
 const NavbarIcon = ({icon, text = 'ERR Text Parameter Undefined'}) => (
 
@@ -24,14 +26,14 @@ class Navbar extends Component {
     return (
         <nav className='bg-slate-200 fixed top-0 left-0 h-screen w-16 flex flex-col
          shadow-lg justify-evenly'>
-            <a href='index.html'>
+            <NavLink to=''>
                 <NavbarIcon icon={<IoMdRose size={IconSize}/>} text='Home'/>
+            </NavLink>
+            <a href='index.html'>
+                <NavbarIcon icon={<MdCode size={IconSize}/>} text='Computer Science'/>
             </a>
             <a href='index.html'>
-                <NavbarIcon icon={<MdCode size={IconSize}/>} text='Code'/>
-            </a>
-            <a href='index.html'>
-                <NavbarIcon icon={<MdMode size={IconSize}/>} text='Writing'/>           
+                <NavbarIcon icon={<MdMode size={IconSize}/>} text='Articles'/>           
             </a>
             <a href='index.html'>
                 <NavbarIcon icon={<MdPalette size={IconSize}/>} text='Art'/>
