1ae35e3d87260ea82cd39b199f29ed0015199d88
rosepetaldrift <rosesophiewright@gmail.com>
2022-05-04 09:16:08 -0500

    Footer update

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -9,11 +9,11 @@ class Landing extends Component{
                 <div className='transition-all duration-200 ease-linear shadow-lg mx-auto max-w-[70vw]   m-[3%] w-auto bg-slate-200 flex rounded-3xl p-[1vw] flex-row-reverse'>
                     <div className=''>
                         <div className='bg-slate-100 p-[3vw] rounded-3xl shadow-lg '>
-                            <h1 className='text-[3vw]	'>Hi, I'm Rose – UI and design system developer.</h1>
+                            <h1 className='text-[4vw]	p-[0vw] m-[0vw]'>Hi, I'm Rose – UI and design system developer.</h1>
                             <br/>
-                            <p className='text-[2.3vw]'>I help people build interactive and compelling user interfaces. I work with a strong focus on simplicity, accessibility and responsive design.</p>
+                            <p className='text-[3vw] p-[0vw] m-[0vw]'>I help people build interactive and compelling user interfaces. I work with a strong focus on simplicity, accessibility and responsive design.</p>
                             <br/>
-                            <a href='index.html' className='underline text-rose-700 italic whitespace-nowrap text-[2vw] hover:bg-rose-100'> Read the full article</a>
+                            <a href='index.html' className='p-[0vw] m-[0vw] underline text-rose-700 italic whitespace-nowrap text-[2vw] hover:bg-rose-100'> Read the full article</a>
 
                         </div>
                         
