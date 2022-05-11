cae2aa11a1f5ae0ab6ed62548c93b277603bbe27
rosepetaldrift <rosesophiewright@gmail.com>
2022-05-11 11:22:00 -0500

    Art fix

--- src/components/Art/Art.js
+++ src/components/Art/Art.js
@@ -3,8 +3,8 @@ import RedWaves from './Art components/RedWaves.js'
 class Art extends Component{
     render() {
         return (
-            <div className=''>
-                <div className='mt-0 w-auto h-auto flex rounded-3xl p-5 flex-col select-none'>
+            <div className='min-h-[100vh]'>
+                <div className='mx-auto w-auto h-auto  flex rounded-3xl p-5 flex-col select-none'>
                     {/*header*/}
                     <h1 className='select-none mx-auto mt-0 relative flex items-center justify-center text-[4vw]  px-9 mb-auto bg-slate-200 text-slate-800 shadow-lg rounded-2xl'>Art </h1>
                 </div>
