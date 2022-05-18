b117c8f824c5f657f3dba03514606a548f5f01e8
Rose <rosesophiewright@gmail.com>
2022-05-17 21:13:04 -0500

    Font Size

--- src/components/Landing/Landing components/Contact/Contact.jsx
+++ src/components/Landing/Landing components/Contact/Contact.jsx
@@ -11,7 +11,7 @@ class Contact extends Component{
                         <p className='ml-10 text-1xl rounded-lg font-bold m-1 italic'><span className='text-main-100'>&#60;</span> <span className='decoration-comp-100 underline-offset-[3.5px] underline'>Contact</span></p>
                         <h1 className='p-10 pt-5 header center rounded-lg font-semibold '>Let's get to work</h1>
                         <div className='ml-10'>
-                        <h1 className='text-1xl rounded-lg m-1 p-1 font-bold select-text'><span className='select-auto underline decoration-main-100 italic font-semibold text-xl'>RoseSophieWright@gmail.com</span> 
+                        <h1 className='text-1xl rounded-lg m-1 p-1 font-bold select-text'><span className='select-auto underline decoration-main-100 italic font-semibold'>RoseSophieWright@gmail.com</span> 
                         </h1>
                         <p className='text-1xl rounded-lg indent-10 m-1 p-1 font-bold select-text'>Want me on your project? Don't be afraid to shoot me an email! I am a freelancer but I am open to offers perfering part-time, and interinship positions. For these inquires lable your email BUSINESS in all caps. Want me to review your source code? Lable your email SOICAL.</p>
                         <br/>
