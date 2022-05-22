ce17578f60b8f93d9973cb01ede62adf5af36577
rosepetaldrift <rosesophiewright@gmail.com>
2022-05-22 11:16:30 -0500

    Left Position change

--- src/components/Landing/Landing components/Scroll Button/Styles.jsx
+++ src/components/Landing/Landing components/Scroll Button/Styles.jsx
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 export const Button = styled.div`
 position: fixed;
 width: 100%;
-left: 80vw;
+left: 90vw;
 bottom: 1vh;
-height: 20%;
-font-size: 3rem;
+height: 10%;
+font-size: 2rem;
 z-index: 100;
 cursor: pointer;
 `
