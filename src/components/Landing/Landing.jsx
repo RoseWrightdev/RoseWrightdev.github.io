bf2d79cada0f08ac1607c18f3f29be33d42d2e1f
rosepetaldrift <rosesophiewright@gmail.com>
2022-06-21 18:50:54 -0500

    Hover effects added

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -17,14 +17,11 @@ class Landing extends Component{
                     <h2 className='text-1xl rounded-lg font-medium m-1'>I work with a strong focus on simplicity, accessibility <br/> and responsive design.</h2>
                     <br/>
                     <NavLink to="/Contact" className='justify-start'>
-                    
 
-                    <a tabIndex={1} className='p-2 mr-auto ml-1 my-1 text-xl font-bold bg-main-100' href='#Contact'>Contact</a>
+                    <a tabIndex={1} className='p-2 mr-auto ml-1 my-1 text-xl font-bold bg-main-100 hover:rounded-lg transition-all' href='#Contact'>Contact</a>
                     </NavLink>
                    
-
-                       <a tabIndex={2} href='#GetToKnowMe' className='p-1'> <button className='p-2 mr-auto ml-1 my-3 text-xl font-bold border-comp-100 border-2' ><span className='whitespace-nowrap'>Get to Know Me</span></button>
-                       </a>
+                    <a tabIndex={2} href='#GetToKnowMe' className='p-1'> <button className='p-2 mr-auto ml-1 my-3 text-xl font-bold border-comp-100 border-2 hover:rounded-lg transition-all' ><span className='whitespace-nowrap'>Get to Know Me</span></button>   </a>
                     </div>
                 </div>
                 <title id='landing'>Two women sitting down and relaxing. As they relax they look foward to the future. Behind these women there is a rose.</title>
